ea63e3f2d06b22574a6acb53115391ce045cca7e
shengze23 <146034179+shengze23@users.noreply.github.com>
2023-11-18 23:45:38 -0600

    Add files via upload

--- partII.m
+++ partII.m
@@ -5,23 +5,25 @@ clear all;
 load('lightField.mat');
 
 % Render image using rays2img
-sensorWidth = 5e-3; 
-numPixels = 200;
+sensorWidth = 0.015; 
+numPixels = 1000;
 [img,~,~] = rays2img(rays(1,:),rays(3,:),sensorWidth,numPixels);
-figure; imagesc(img); axis image; title('Raw rays'); 
+figure; imshow(img); axis image; title('Raw rays'); 
 
 % Propagate rays by distance d
-d = 0.1; % propagation distance
+d = 0.4; % propagation distance
 Md = [1 d 0 0; 0 1 0 0; 0 0 1 d; 0 0 0 1];
 raysProp = Md * rays; 
 
 % Render propagated rays
 [img,~,~] = rays2img(raysProp(1,:),raysProp(3,:),sensorWidth,numPixels);
-figure; imagesc(img); axis image; title('Propagated rays');
+figure; imshow(img); 
+axis image; 
+title('Propagated rays');
 
 % Imaging system parameters
-f = 0.5; 
-d2 = 1;
+f = 54/215; 
+d2 = 0.675;
 
 % Ray transfer matrices
 Mf = [1 0 0 0; -1/f 1 0 0; 0 0 1 0; 0 0 -1/f 1]; 
@@ -32,4 +34,8 @@ raysImaged = Md2*Mf*rays;
 
 % Render imaged rays
 [img,~,~] = rays2img(raysImaged(1,:),raysImaged(3,:),sensorWidth,numPixels);
-figure; imagesc(img); axis image; title('Imaged rays');
+figure; 
+img=flip(img, 2); 
+imshow(img); 
+axis image; 
+title('Imaged rays');
